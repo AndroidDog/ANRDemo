2358b44373fa8fcb297f128365e44cef79cbdbf6
xtsun <xtsun@iflytek.com>
2018-03-29 14:06:35 +0800

    feat:新增ANR类型测试

--- app/src/main/java/com/example/sunxt/anrdemo/MainActivity.java
+++ app/src/main/java/com/example/sunxt/anrdemo/MainActivity.java
@@ -1,13 +1,168 @@
 package com.example.sunxt.anrdemo;
 
+import android.content.res.AssetManager;
+import android.graphics.Matrix;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AnrDemo";
+
+    private static final String GRADLE_TASK_TREE_LOG = "task-tree.log";
+
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 主线程慢代码
+        Button slowCodeMainThread = findViewById(R.id.btn_slow_code_main_thread);
+        slowCodeMainThread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                slowCode();
+//                new AsyncTask<Integer, Integer, Matrix>() {
+//
+//                    @Override
+//                    protected Matrix doInBackground(Integer... integers) {
+//                        slowCode();
+//                        return null;
+//                    }
+//                }.execute(0);
+            }
+        });
+
+        // 主线程io
+        Button ioMainThread = findViewById(R.id.btn_io_main_thread);
+        ioMainThread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 100; i++) {
+                    copyFile();
+                }
+            }
+        });
+
+        Button lockContention = findViewById(R.id.btn_lock_contention);
+        lockContention.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        synchronized (lock1) {
+                            try {
+                                Thread.sleep(10000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (lock1) {
+                    Log.i(TAG, "main thread get lock1");
+                }
+            }
+        });
+
+        // 死锁
+        Button deadLockBtn = findViewById(R.id.btn_deadlock);
+        deadLockBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                synchronized (lock1) {
+                    Log.i(TAG, "main thread got lock1");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock2) {
+                        Log.i(TAG, "main thread got lock2");
+                    }
+                }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        synchronized (lock2) {
+                            Log.i(TAG, "worker thread got lock2");
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                            synchronized (lock1) {
+                                Log.i(TAG, "worker thread got lock1");
+                            }
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+
+
+    private void slowCode() {
+        Log.i(TAG, "slow code on main thread, start at :" + System.currentTimeMillis());
+        int i = 0;
+        Matrix matrix = new Matrix();
+        matrix.setValues(new float[]{100f, 110f, 120f, 130f, 140f, 150f, 160f, 170f, 180f});
+        while (i < Integer.MAX_VALUE) {
+            matrix.postRotate(30f, 10f, 10f);
+        }
+        Log.i(TAG, "slow code on main thread, end at :" + System.currentTimeMillis());
+    }
+
+    private void copyFile() {
+        Log.i(TAG, "io on main thread, start at :" + System.currentTimeMillis());
+        InputStream inputStream = null;
+        FileOutputStream fileOutputStream = null;
+        try {
+            AssetManager assetManager = getAssets();
+            inputStream = assetManager.open(GRADLE_TASK_TREE_LOG);
+            fileOutputStream = new FileOutputStream(new File(Environment.getExternalStorageDirectory() + File.separator + "task-tree.log"));
+            byte[] buffer = new byte[1024];
+            int byteCount = 0;
+            while ((byteCount = inputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, byteCount);
+            }
+            fileOutputStream.flush();//刷新缓冲区
+        } catch (IOException e) {
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                }
+                inputStream = null;
+            }
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                }
+                fileOutputStream = null;
+            }
+        }
+        Log.i(TAG, "io on main thread, end at :" + System.currentTimeMillis());
     }
 }
